e88d0c239f33a6f0edbde38dd9c456a225c147fd
郭宇 <guoyu8@xiaomi.com>
2020-04-26 15:00:08 +0800

    feature: add global typeFile

---
+++ src/types/index.ts
@@ -0,0 +1,15 @@
+// 项目中所有公共类型定义文件
+export type Method = 'get' | 'GET'
+| 'delete' | 'DELETE'
+| 'head' | 'HEAD'
+| 'options' | 'OPTIONS'
+| 'post' | 'POST'
+| 'put' | 'PUT'
+| 'patch' | 'PATCH'
+
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+}
