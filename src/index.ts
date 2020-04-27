d725d5bb366308a57839e26fff49103a2aad2a15
郭宇 <guoyu8@xiaomi.com>
2020-04-27 20:37:49 +0800

    feature: headers

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+// 入口文件
 import xhr from './xhr'
 import { buildURL } from './helpers/url'
 import { AxiosRequestConfig } from './types/index';
 import { transformRequest } from './helpers/data';
+import { processHeaders } from './helpers/headers';
 
 
 function axios(config: AxiosRequestConfig): void {
@@ -11,6 +13,8 @@ function axios(config: AxiosRequestConfig): void {
 
 function processConfig(config: AxiosRequestConfig): void {
   config.url = transformURL(config)
+  // 注意调用顺序
+  config.headers = transformHeaders(config)
   config.data = transformRequestData(config)
 }
 
@@ -21,6 +25,14 @@ function transformURL(config: AxiosRequestConfig): string {
 // 上面函数是对config.url做了处理，下面对config.data做处理
 function transformRequestData(config: AxiosRequestConfig): any {
   return transformRequest(config.data)
+}
+
+
+function transformHeaders(config: AxiosRequestConfig): any {
+  // headers给个默认值{},不为空即可，不然processHeaders处理会出问题
+  // 解构赋值默认值的写法
+  const { headers = {}, data } = config
+  return processHeaders(headers, data)
 }
 
 
--- src/xhr.ts
+++ src/xhr.ts
@@ -3,9 +3,19 @@ import { AxiosRequestConfig } from './types'
 
 export default function xhr(config: AxiosRequestConfig): void {
   // 因为data、method是可选
-  const { data = null, url, method = 'get' } = config
+  const { data = null, url, method = 'get', headers } = config
   const request = new XMLHttpRequest()
   // true: 异步
-  request.open(method.toUpperCase(), url, true)
+  request.open(method.toUpperCase(), url, true) // true 表示是否异步
+
+  Object.keys(headers).forEach(name => {
+    if (data === null && name.toLocaleLowerCase() === 'content-type') {
+      delete headers[name]
+    } else {
+      request.setRequestHeader(name, headers[name])
+    }
+  })
+
+
   request.send(data)
 }
