12389b19238099d516f9221a457baadf6af9d3cf
郭宇 <guoyu8@xiaomi.com>
2020-04-27 15:20:06 +0800

    feature: config header content-type

--- README.md
+++ README.md
@@ -7,3 +7,21 @@ send方法的参数支持Document 和BodyInit类型
 BodyInit包括：Blob, BufferSource, FormData, URLSearchParams, ReadableStream, USVString
 当没有数据时，我们还可以传入null
 如果传入的data是个普通json，要转化成字符串JSON字符串
+
+## 处理请求Header
+我们做了请求数据的处理，把data转换成了JSON字符串，但是数据到达服务端的时候，服务端并不能正常解析
+我们发送的数据，因为我们没有给请求 headr 设置正则的 Content-Type
+所以，我们发送请求的时候，可以支持配置 headers 属性，如下：
+axios({
+  method: 'post',
+  url: '/base/post',
+  headers: {
+    'content-type': 'application/json;charset=utf8'
+  },
+  data: {
+    a: 1,
+    b: 2
+  }
+})
+并且，在当我们传入的data为普通对象的时候， headers 如果没有配置 Content-Type属性，需要自动设置请求 header 的
+Content-Type 字段为： application/json;charset=utf-8
---
+++ src/helpers/headers.ts
@@ -0,0 +1,24 @@
+import { isPlainObject } from './utils';
+
+// 因为headers里面的Content-Type大小写不敏感，都对，你传的content-type，但检测的时候用了Content-Type就会出问题
+// 所以，这里规范化一下
+function normalizeHeaderName(headers: any, normalizedName: string): void {
+  if (!headers) {
+    return
+  }
+  Object.keys(headers).forEach(name => {
+    if (name !== normalizedName &&  name.toUpperCase() === normalizedName.toUpperCase()) {
+      headers[normalizedName] = headers[name]
+      delete headers[name]
+    }
+  })
+}
+export function processHeaders(headers: any, data: any): any {
+  normalizeHeaderName(headers, 'Content-Type')
+  // 传入data，是为了判断data是不是个普通对象，如果是普通对象，添加Content-Type为json
+  if (isPlainObject(data)) {
+    if (headers && !headers['Content-Type']) {
+      headers['Content-Type'] = 'application/json;charset=utf-8'
+    }
+  }
+}
